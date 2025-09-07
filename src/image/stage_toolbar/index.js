0715faf2388c5117f8539e13dee72b4a8354eca9
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-09-08 01:10:17 +0900

    Add files via upload

---
+++ src/image/stage_toolbar/index.js
@@ -0,0 +1,39 @@
+import stroke from './stroke.svg';
+import rect from './rect.svg';
+import draw from './draw.svg';
+import erase from './erase.svg';
+import cut from './cut.svg';
+import select from './select.svg';
+import globalErase from './global_erase.svg';
+import top from './top.svg';
+import resize from './resize.svg';
+import undo from './undo.svg';
+import redo from './redo.svg';
+import path from './path.svg';
+import direction from './direction.svg';
+import relay from './relay.svg';
+import border from './border.svg';
+import expand from './expand.svg';
+import settings from './settings.svg';
+import wand from './wand.svg';
+
+export default {
+  stroke,
+  rect,
+  draw,
+  erase,
+  cut,
+  select,
+  globalErase,
+  top,
+  path,
+    direction,
+    relay,
+    border,
+    expand,
+    wand,
+  resize,
+  undo,
+  redo,
+  settings,
+};
